7999805365cad8a4d4dea42cb887e6922aa24aa0
wrathchild14 <jovanprodanov123@gmail.com>
2022-09-16 22:05:26 -0700

    Creating simple window

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -2,7 +2,29 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-int main() 
-{
+int main() {
+	if (!glfwInit()) {
+		std::cout << "Failed to init GLFW" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
