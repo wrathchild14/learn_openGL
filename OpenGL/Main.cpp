5f5f1ae0947f21d9edd2d853e57f407ac83a09a9
wrathchild14 <jovanprodanov123@gmail.com>
2022-09-17 00:50:41 -0700

    Add color to the window buffer

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -8,8 +8,10 @@ int main() {
 		return -1;
 	}
 
+	// What version of openGL we are using
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Using the core profile, so that means we only have modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL", NULL, NULL);
@@ -18,7 +20,17 @@ int main() {
 		glfwTerminate();
 		return -1;
 	}
+	// Introduce the window in the current context
 	glfwMakeContextCurrent(window);
+
+	// Load glad with openGL configuration
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glfwSwapBuffers(window);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
