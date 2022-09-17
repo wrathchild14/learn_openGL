5e88d0f9c17eb6c6d2aa1157b3030ac4bae2e930
wrathchild14 <jovanprodanov123@gmail.com>
2022-09-17 15:29:11 -0700

    Separate with classes

---
+++ OpenGL/VertexArray.h
@@ -0,0 +1,18 @@
+#ifndef VAO_CLASS_H
+#define VAO_CLASS_H
+
+#include<glad/glad.h>
+#include"VertexBuffer.h"
+
+class VertexArray {
+	public:
+		GLuint ID;
+		VertexArray();
+
+		void LinkVertexBuffer(VertexBuffer VBO, GLuint layout);
+		void Bind();
+		void Unbind();
+		void Delete();
+};
+
+#endif // !VAO_CLASS_H
