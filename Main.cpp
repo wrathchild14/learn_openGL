4848998442c32172e578e7351f6737782877c990
wrathchild14 <jovanprodanov123@gmail.com>
2022-10-13 23:09:39 +0200

    Add white cube for lights

--- Main.cpp
+++ Main.cpp
@@ -64,6 +64,34 @@ int main()
 		3, 0, 4
 	};
 
+	GLfloat light_vertices[] =
+	{ //     COORDINATES     //
+		-0.1f, -0.1f,  0.1f,
+		-0.1f, -0.1f, -0.1f,
+		 0.1f, -0.1f, -0.1f,
+		 0.1f, -0.1f,  0.1f,
+		-0.1f,  0.1f,  0.1f,
+		-0.1f,  0.1f, -0.1f,
+		 0.1f,  0.1f, -0.1f,
+		 0.1f,  0.1f,  0.1f
+	};
+
+	GLuint light_indices[] =
+	{
+		0, 1, 2,
+		0, 2, 3,
+		0, 4, 7,
+		0, 7, 3,
+		3, 7, 6,
+		3, 6, 2,
+		2, 6, 5,
+		2, 5, 1,
+		1, 5, 4,
+		1, 4, 0,
+		4, 5, 6,
+		4, 6, 7
+	};
+
 	Shader shader_program("default.vert", "default.frag");
 
 	VertexArray vertex_array_object;
@@ -81,6 +109,32 @@ int main()
 	vertex_array_object.Unbind();
 	vertex_buffer_object.Unbind();
 	element_buffer_object.Unbind();
+
+	// Lights shader
+	Shader light_shader("light.vert", "light.frag");
+
+	VertexArray light_vertex_array;
+	light_vertex_array.Bind();
+
+	VertexBuffer light_buffer_object(light_vertices, sizeof(light_vertices));
+	ElementBuffer light_element_object(light_indices, sizeof(light_indices));
+
+	light_vertex_array.LinkAttrib(light_buffer_object, 0, 3, GL_FLOAT, 3 * sizeof(GL_FLOAT), (void*)0);
+
+	// Hard coded setting values
+	glm::vec3 light_pos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 light_model = glm::mat4(1.0f);
+	light_model = glm::translate(light_model, light_pos); // Gives the matrix a position
+
+	glm::vec3 pyramid_pos = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::mat4 pyramid_model = glm::mat4(1.0f);
+	pyramid_model = glm::translate(pyramid_model, pyramid_pos);
+
+	light_shader.Activate();
+	glad_glUniformMatrix4fv(glGetUniformLocation(light_shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(light_model));
+	shader_program.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(shader_program.ID, "model"), 1, GL_FALSE, glm::value_ptr(pyramid_model));
+
 
 	// Texture
 	Texture cat_texture("square_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
@@ -105,7 +159,13 @@ int main()
 		cat_texture.Bind();
 		vertex_array_object.Bind();
 
-		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, nullptr); // Draw elements from the EBO
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, nullptr); // Draw elements from the EBO
+
+		light_shader.Activate();
+		camera.Matrix(light_shader, "camMatrix");
+		light_vertex_array.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(light_indices) / sizeof(int), GL_UNSIGNED_INT, nullptr);
+
 		glfwSwapBuffers(window); // Swap the back buffer with the front buffer
 		glfwPollEvents(); // Takes care of all glfw events
 	}
